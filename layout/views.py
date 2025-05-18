b46b345ea44015a6bf9a9f206267a02249327a0d
ReDiego0 <134623797+ReDiego0@users.noreply.github.com>
2025-05-18 02:07:22 -0400

    NO SE SUBIO NIUNA WEA EN EL COMMIT ANTERIOR

---
+++ layout/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+
+def homepage(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
+
+def redirect_homepage(request):
+    return redirect('homepage')
+
+# Create your views here.
