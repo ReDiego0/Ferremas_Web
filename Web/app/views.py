501f9c0ac5af5fb2fa2cf39e9fec8bb2f47476e7
ReDiego0 <134623797+ReDiego0@users.noreply.github.com>
2025-05-27 05:37:28 -0400

    Add product management features including forms, views, and templates

--- Web/app/views.py
+++ Web/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductoForm
 
-# Create your views here.
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_producto')  # Redirige a la misma página o a otra vista
+    else:
+        form = ProductoForm()
+    
+    return render(request, 'agregar_producto.html', {'form': form})
+
+
